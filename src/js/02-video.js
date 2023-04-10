428f10fcb74a13f1aadd81a8263a55fc378b2154
Merlin <merlin.vop@gmail.com>
2023-04-10 21:29:48 +0300

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
-console.log('Task 2');
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe#vimeo-player');
+const player = new Player(iframe);
+
+const playerCurrentTime = 'videoplayer-current-time';
+const lastTime = localStorage.getItem(playerCurrentTime);
+
+const onPlay = function (timeupdate) {
+  localStorage.setItem(playerCurrentTime, timeupdate.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player
+  .setCurrentTime(lastTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
