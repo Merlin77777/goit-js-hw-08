2d35d7498231236d7a8423976d3e824fc8307848
Merlin <merlin.vop@gmail.com>
2023-04-10 22:34:49 +0300

    Task 3_

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,6 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
+form.addEventListener('input', onStore);
 form.addEventListener('submit', onSubmit);
 
 const keyFormState = 'feedback-form-state';
@@ -9,17 +10,16 @@ let formValues = JSON.parse(localStorage.getItem(keyFormState));
 
 const { email, message } = form.elements;
 
-const onStore = throttle(event => {
+function onStore() {
   formValues = { email: email.value, message: message.value };
   localStorage.setItem(keyFormState, JSON.stringify(formValues));
-}, 500);
+}
+throttle(onStore, 500);
 
 if (formValues) {
   email.value = formValues.email;
   message.value = formValues.message;
 }
-
-form.addEventListener('input', onStore);
 
 function onSubmit(event) {
   event.preventDefault();
