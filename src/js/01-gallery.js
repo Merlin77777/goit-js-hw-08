409787a35f5c4d2bdc83cebb930d2e0cc5a1cd58
Merlin <merlin.vop@gmail.com>
2023-04-10 21:15:27 +0300

    Task 1 ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+ console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+
+function marckup(arr) {
+  const htmltext = arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>
+    </li> `
+    )
+    .join('');
+  return htmltext;
+}
+
+container.insertAdjacentHTML('beforeend', marckup(galleryItems));
+
+let modalWindow = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,1 @@
-
+console.log('Task 2');
